9ec868f46dbefe75517ed4e34a261b04c97a2d66
caprinavaldez <caprinavaldez@gmail.com>
2018-01-28 16:00:22 -0800

    had to fix heroku deploy

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Chile Relleno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Burger');
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+//import orm.js into burger.js
+//create the code that will call the ORM functions 
+//using burger specific input for the ORM.
+//Export at the end of this file.
+
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+          cb(res);
+        });
+    },
+      // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res){
+          cb(res);
+      });
+    }
+};
+    
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;   
